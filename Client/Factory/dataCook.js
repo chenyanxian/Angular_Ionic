970fbf524d7f5079877b5a932bcc4cb173af8b11
Stringbao <String_bao@163.com>
2015-12-29 17:10:12 +0800

    edit article API

--- Client/Factory/dataCook.js
+++ Client/Factory/dataCook.js
@@ -8,7 +8,9 @@ angular.module("ionicApp").service("dataTool",function(){
     //key and val
 
     var map = {
-        user:"user"
+        user:"user",
+        blog:"blog",
+        category:"category"
     }
 
     function getItemByKey(key,data){
@@ -38,8 +40,29 @@ angular.module("ionicApp").service("dataTool",function(){
             return getItemByKey(map.user,this.data);
         }
 
+        //保留原数据不会被污染,因此存的是字符串值类型,而不是对象引用类型
+        this.setBlogData = function(data){
+            var tmp = {key:map.blog,data:JSON.stringify(data)};
+            this.overWrite(map.blog,tmp);
+        }
+
+        this.getBlogs = function(){
+            return getItemByKey(map.blog,this.data);
+        }
+
+        this.setCategory = function(data){
+            var tmp = {key:map.category,data:data};
+            this.overWrite(map.category,tmp);
+        }
+
+        this.getCategory = function(){
+            return getItemByKey(map.category,this.data);
+        }
+
         this.overWrite = function(key,data){
+
             var res = {tag:false,data:null};
+
             for(var i =0;i<this.data.length;i++){
                 if(this.data[i].key == key){
                     res.tag = true;
@@ -50,6 +73,7 @@ angular.module("ionicApp").service("dataTool",function(){
             if(!res.tag){
                 this.data.push(data);
             }
+
         }
     }
 
